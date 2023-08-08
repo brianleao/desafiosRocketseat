ea7af060f76ee91d466b3df5e28076d8ad65dbf8
brianleao <brianleao@icloud.com>
2023-08-08 16:26:09 -0300

    Countdown finished with minutes and seconds

---
+++ ThemeSwitcher/script.js
@@ -0,0 +1,9 @@
+function trocarTema() {
+  const html = document.documentElement
+  
+  if(html.classList.contains('dark')) {
+  html.classList.remove('dark');
+  } else {
+  html.classList.add('dark');
+  }
+}
