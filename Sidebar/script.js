5ae4d5a93e8d285369117ce4234f4d9dec119262
brianleao <brianleao@icloud.com>
2023-08-12 14:18:32 -0300

    project 80% finished

--- Sidebar/script.js
+++ Sidebar/script.js
@@ -1,4 +1,7 @@
-function openSidebar (){
-  let col = document.querySelector('.coluna');
-  col.style.background = "yellow"
-}
+let botaoExp = document.querySelector('#icon-menu')
+let menuSide = document.querySelector('.coluna-lateral')
+
+botaoExp.addEventListener('click', function(){
+  //o classList.toggle serve para se, não existir uma classe eu quero que ela seja criada e se existir, quero que ela seja removida.
+  menuSide.classList.toggle('expandir');
+})
---
+++ Sidebar2/script.js
@@ -0,0 +1,4 @@
+function openSidebar (){
+  let col = document.querySelector('.coluna');
+  col.style.background = "yellow"
+}
