46c8e848313edc92d00e54d1ec77de97f8d23e3b
brianleao <brianleao@icloud.com>
2023-08-12 20:04:13 -0300

    code finished

--- Sidebar/script.js
+++ Sidebar/script.js
@@ -4,4 +4,5 @@ let menuSide = document.querySelector('.coluna-lateral')
 botaoExp.addEventListener('click', function(){
   //o classList.toggle serve para se, não existir uma classe eu quero que ela seja criada e se existir, quero que ela seja removida.
   menuSide.classList.toggle('expandir');
+  
 })
